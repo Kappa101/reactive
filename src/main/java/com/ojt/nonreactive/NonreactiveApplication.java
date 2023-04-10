14cc2cdc280a25dc993fd6f562ff0df567d828a4
Chandra Prakash <kcp.masters@gmail.com>
2023-04-10 21:36:33 +0530

    first commit

---
+++ src/main/java/com/ojt/nonreactive/NonreactiveApplication.java
@@ -0,0 +1,13 @@
+package com.ojt.nonreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonreactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonreactiveApplication.class, args);
+	}
+
+}
